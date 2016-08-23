0f92965269cd8e31292637b8b1459d5a0a28f89f
jordanallain <jallain@gmail.com>
2016-08-23 14:21:48 -0400

    Change URL back to Heroku
    Chang URL in app.js to Heroku

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,8 @@
 
 //declare variable app with key api that has a value of our server location
 const app = {
-  // api: 'https://allballers.herokuapp.com/',
-  api: 'http://localhost:3000/'
+  api: 'https://allballers.herokuapp.com/',
+  // api: 'http://localhost:3000/'
 };
 
 //exports the variable containing the server location to be used in other files.
