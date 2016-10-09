8873f60d78096bfe16542c805a0feb96e09d2e40
jordanallain <jallain@gmail.com>
2016-10-08 20:54:41 -0400

    Redeploy after styling
    Major styling done attempting to redeploy.

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(47);
+	module.exports = __webpack_require__(50);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 47:
+/***/ 50:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
